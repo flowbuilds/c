9129a5a12584b04dd73cb732beaecf8d913d814b
Flowbuilds <83973323+flowbuilds@users.noreply.github.com>
2021-09-05 12:53:55 +0100

    Update and rename bvls10.js to bvls11.js

--- bvls11.js
+++ bvls11.js
@@ -195,6 +195,7 @@ function lsDateCheck(lsId, e) {
 					if(c.hasOwnProperty("events")) {
 						c.events.forEach(d => {
 							let e = [new Date(d.start_time), new Date(d.end_time)];
+							console.log(e);
 							let x = [ls.activeFilters.start, ls.activeFilters.end];
 							x.forEach(y => {
 								if(y >= e[0] && y < e[1]) {console.log("UNAVAILABLE"); ac = false; return}});
