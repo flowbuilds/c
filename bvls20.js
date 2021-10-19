243dfc091d9d34b418d7bc41dcd6439a3a04d27f
Flowbuilds <83973323+flowbuilds@users.noreply.github.com>
2021-10-19 12:16:21 +0100

    Update and rename bvls19.js to bvls20.js

--- bvls20.js
+++ bvls20.js
@@ -184,12 +184,10 @@ function lsDateCheck(lsId, e) {
 		if(ls.id == lsId && ls.hasOwnProperty("data") && e.querySelector("[data-ls-filter-name]")) {
 			let a = e.querySelector("[data-ls-filter-name]").getAttribute("data-ls-filter-name"), b;
 			a = a.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
-			console.log(a);
 			if(ls.data.hasOwnProperty("properties")) {
 				ls.data.properties.every(c => {
 					let d = c.name.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
 					if(d == a) {
-						console.log(d)
 						b = c.calendars;
 						return false
 					}
@@ -242,7 +240,6 @@ function lsListingCheck(lsId) {
 							if(a instanceof Date) {
 								if(z == "start" && ls.activeFilters.hasOwnProperty("end")) {
 									x = lsDateCheck(lsId, e);
-									console.log(x)
 								}
 							}
 							else if(y.hasAttribute("data-ls-filter-" + z)) {
